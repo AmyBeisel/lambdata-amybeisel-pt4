fae3b2bb54d26ae8e2019303cb3c22341eea39dc
Amy Beisel <amy.beisel@gmail.com>
2020-03-13 20:59:18 -0600

    Setup pipenv environment

--- amys_lambda/my_script.py
+++ amys_lambda/my_script.py
@@ -1,11 +1,11 @@
 
 #amys_lambda/my_script.py
 
-import pandas 
+import pandas as pd
 
 print("Happy Tuesday Night.")
 
-df = pandas.DataFrame({"x": [1,2,3], "y": [4,5,6]})
+df = pd.DataFrame({"x": [1,2,3], "y": [4,5,6]})
 print(df.head())
 
 
