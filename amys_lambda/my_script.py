90444ca4a5606b09a928cde6f3398ed833e65d69
Amy Beisel <amy.beisel@gmail.com>
2020-03-13 21:29:02 -0600

    added init file

--- amys_lambda/my_script.py
+++ amys_lambda/my_script.py
@@ -3,11 +3,13 @@
 
 import pandas as pd
 
+from amys_lambda.my_mod import enlarge
+
 print("Happy Tuesday Night.")
 
 df = pd.DataFrame({"x": [1,2,3], "y": [4,5,6]})
 print(df.head())
 
-
-
+x = 5
+print("ENLARGE", x, "TO", enlarge(x))
 
