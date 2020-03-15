0bf91dfd443bd2e3db2a89b862051790aebf265b
Amy Beisel <amy.beisel@gmail.com>
2020-03-15 14:44:37 -0600

    Push

--- amys_lambda/my_mod.py
+++ amys_lambda/my_mod.py
@@ -3,3 +3,14 @@
 def enlarge(n):
     return n * 100
 
+
+
+# if __name__ == "__main__":
+#     #only if running from command line, invoke the following code:
+#     #otherwise, don't 
+
+# print("JUNK")
+# print("GLOBAL SCOPE")
+
+# y = float(input("Please input a number to enlarge: "))
+# print(enlarge(y))
--- amys_lambda/my_script.py
+++ amys_lambda/my_script.py
@@ -2,7 +2,6 @@
 #amys_lambda/my_script.py
 
 import pandas as pd
-
 from amys_lambda.my_mod import enlarge
 
 print("Happy Tuesday Night.")
@@ -10,6 +9,8 @@ print("Happy Tuesday Night.")
 df = pd.DataFrame({"x": [1,2,3], "y": [4,5,6]})
 print(df.head())
 
+#test 1: from lecture. Make sure thing are working 
 x = 5
 print("ENLARGE", x, "TO", enlarge(x))
+
 
