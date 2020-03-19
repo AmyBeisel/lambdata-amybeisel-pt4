c6d004b4131a49f31db3c41fe177b92c9ed18db7
Amy Beisel <amy.beisel@gmail.com>
2020-03-18 19:42:02 -0600

    refactor to a OOP approach

--- amys_lambda/assignment.py
+++ amys_lambda/assignment.py
@@ -6,40 +6,53 @@ import pandas as pd
 #take a pandas DF that has a state abbrevations
 #....and write a function to add corresponding state names
 
-def add_state_name(my_df):
-    """
-    Adds corresponding state names to dataframe. 
-    Param: my_df (pd.DataFrame) containing a column called "abbrevations"
-    """
-    
-    new_df = my_df.copy()
-    #type(my_df["abbrevations"]) #> <class 'pandas.core.series.Series'>
-    #see: https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.Series.map.html
+class DataProcessor():
+    def __init__(self, my_df):
+        """
+        Param: my_df (pd.DataFrame) containing a column called "abbrevations"
 
-    names_map = {
-        "CA":  "Califorina",
-        "CO":  "Colorado",
-        "CT":  "Connecticut",
-        "TX":  "Texas",
-        "DC":  "Dist. of Columbia",
-        "OH": "Ohio",
-        "MI": "Michigan"
-    }
+        """
+        self.df = my_df.copy()
+
+    def add_state_name(self):
+        """
+        Adds corresponding state names to dataframe. 
+        Param: my_df (pd.DataFrame) containing a column called "abbrevations"
+        """
+        
+        #new_df = self.df.copy()
+        #type(my_df["abbrevations"]) #> <class 'pandas.core.series.Series'>
+        #see: https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.Series.map.html
+
+        names_map = {
+            "CA":  "Califorina",
+            "CO":  "Colorado",
+            "CT":  "Connecticut",
+            "TX":  "Texas",
+            "DC":  "Dist. of Columbia",
+            "OH": "Ohio",
+            "MI": "Michigan",
+            'PA': 'Pennsylvania'
+        }
     
-    new_df["name"] = new_df["abbrevations"].map(names_map)
-    return new_df
+        self.df["name"] = self.df["abbrevations"].map(names_map)
+    
 
 
 if __name__ == "__main__":
     pass
     print("------------------------")
-    df = pd.DataFrame({"abbrevations": ["CA", "CO", "CT", "TX", "DC"]})
-    print(df.head())
-    new_df = add_state_name(df)
-    print(new_df.head())
+    df1 = pd.DataFrame({"abbrevations": ["CA", "CO", "CT", "TX", "DC"]})
+    #print(df.head())
+    # new_df = add_state_name(df1)
+    # print(new_df.head())
+    processor = DataProcessor(df1)
+    print(processor.df.head())
+    processor.add_state_name()
+    print(processor.df.head())
 
-    print("------------------------")
-    df2 = pd.DataFrame({"abbrevations": ["OH", "MI", "CT", "TX", "PA"]})
-    print(df2.head())
-    new_df2 = add_state_name(df2)
-    print(new_df2.head())
+    # print("------------------------")
+    # df2 = pd.DataFrame({"abbrevations": ["OH", "MI", "CT", "TX", "PA"]})
+    # print(df2.head())
+    # new_df2 = add_state_name(df2)
+    # print(new_df2.head())
